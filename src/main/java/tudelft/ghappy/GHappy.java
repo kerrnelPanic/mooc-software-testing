edafc7d3643adb7c4b7319cd7f18b9d34562ea37
Baldur Kellner <akwegr@gmx.net>
2020-10-18 20:52:19 +0200

    GHappy with test and annotations for code improvements

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -3,15 +3,17 @@ package tudelft.ghappy;
 public class GHappy {
 
     public boolean gHappy(String str) {
+        //at the moment, our method just fails on null
         assert str!=null;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
+                //here's the bug, we try to access index -1 of the char array
                 if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
         }
-
+        //we reach this statement also if there is no g in the evaluated string
         return true;
 
     }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,44 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    // what to do if no g's at all?
+    @ParameterizedTest(name = "{0}: {1} => {2}")
+    @CsvSource({
+            // we do not know what to do if there is no g in the string or no string at all
+            // at the moment, our method fails if supplied with null and evaluates to true if no g in string
+            // I'd suppose, it should evaluate to false or return null if there is no g at all
+            // str is null
+            "'str-is-null', , 'f'",
+            // loop is not executed at all
+            "'str-length-zero','', 'f'",
+            // loop is executed once
+            "'str-length-one','a', 'f'",
+            // loop is executed several times
+            "'str-length-many','abc', 'f'",
+
+            // one single g
+            "'one-single-g', 'xgx', 'f'",
+            // two single g
+            "'two-single-g', 'xgxgx', 'f'",
+            // g at fist and last position of list
+            "g-at-first/last-position', 'gxg', 'f'",
+            // one single g and one double g
+            "'single-g-and-double-g', 'xgxggx', 'f'",
+            // one double g only
+            "'double-g-only', 'xggx', 't'"
+
+    })
+    public void testAlgorithm(String partition, String inputString, Character expectedResult) {
+        boolean result = new GHappy().gHappy(inputString);
+        Character resultString = result ? 't' : 'f';
+        Assertions.assertEquals(expectedResult , resultString);
+    }
+
+
 }
